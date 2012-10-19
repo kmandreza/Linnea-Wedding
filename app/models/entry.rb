175c90c69ffdd4f159dc45f1e958e9de694c0e9f
Khara Muniz <kmandreza@gmail.com>
2012-10-18 19:30:15 -0700

    Linnea's Wedding

---
+++ app/models/entry.rb
@@ -0,0 +1,3 @@
+class Entry < ActiveRecord::Base
+  attr_accessible :name, :text
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guestbook::Application.config.secret_token = '661388a794af323efd48d6a6165a72ea34d1640f52d5d0b20c1cf0e16c45e97ed50cd5d7d4a81baf29614b2d843c02bf619f7d763299f84290266bac374c1947'
