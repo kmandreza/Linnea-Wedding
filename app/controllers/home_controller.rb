84cbc9b3838da8322f0c4c2200eac76d342d56b8
Khara Muniz <kmandreza@gmail.com>
2012-10-22 23:18:16 -0700

    Major updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,31 @@
 class HomeController < ApplicationController
   def index
   end
+  
+  def story
+  end
+  
+  def bparty
+  end
+  
+  def wday
+  end
+  
+  def accommodations
+  end
+  
+  def travel
+  end
+  
+  def registry
+  end
+  
+  def gb
+  end
+  
+  def contact
+  end
+  
+  def address
+  end
 end
